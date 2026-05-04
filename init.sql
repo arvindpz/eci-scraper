c560e774ceac90b7e0571d37370af10d099bfacd
arvindpz <parvind97@gmail.com>
2026-05-04 19:30:22 +0530

    Modify table name

--- init.sql
+++ init.sql
@@ -1,9 +1,9 @@
-DROP table t315;
-CREATE TABLE t315 as SELECT * from read_csv('data/results.csv', header=True);
+DROP table results;
+CREATE TABLE results as SELECT * from read_csv('data/results.csv', header=True);
 
-select count(*) as "Leading In", "Leading Party" from t315 where Status = 'Result in Progress' group by "Leading Party" order by "Leading In";
-select count(*) as "Trailing In", "Trailing Party" from t315 where Status = 'Result in Progress' group by "Trailing Party" order by "Trailing In";
+select count(*) as "Leading In", "Leading Party" from results where Status = 'Result in Progress' group by "Leading Party" order by "Leading In";
+select count(*) as "Trailing In", "Trailing Party" from results where Status = 'Result in Progress' group by "Trailing Party" order by "Trailing In";
 
-select Constituency, "Leading Party", Margin, "Trailing Party", Round from t315 where Margin < 1000 order by Margin;
+select Constituency, "Leading Party", Margin, "Trailing Party", Round from results where Margin < 1000 order by Margin;
 
-select count(*) as "Constituencies won", "Leading Party" from t315 where Status != 'Result in Progress' group by "Leading Party" order by "Constituencies won" desc;
+select count(*) as "Constituencies won", "Leading Party" from results where Status != 'Result in Progress' group by "Leading Party" order by "Constituencies won" desc;
